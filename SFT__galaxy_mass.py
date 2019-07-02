612ff9e3718b99b52f097469a0ba1ae975e244c3
zhiqiangyan <beijingjuzikong@gmail.com>
2019-07-02 16:34:15 +0200

    update

--- SFT__galaxy_mass.py
+++ SFT__galaxy_mass.py
@@ -106,23 +106,23 @@ if __name__ == '__main__':
     # galevo.generate_SFH(SFH_shape, Log_SFR, SFEN, sfr_tail, skewness, location)
     # simulate(imf, Log_SFR, SFEN, STF)
 
-    # # a_pipeline
-    # # igimf   1.0 2.0 3.0            50   (0.2 0.3) 0.4 1.0 1.4 1.5
-    # # generate SFH:
-    # SFEN = 100  # Parallelizing only work for the same SFEN since SFH.txt file is the same!
-    # SFH_shape = 'flat'
-    # location = 0
-    # skewness = 10
-    # sfr_tail = 0
-    # Log_SFR_list = [1.0, 2.0]
-    # for Log_SFR in Log_SFR_list:
-    #     galevo.generate_SFH(SFH_shape, Log_SFR, SFEN, sfr_tail, skewness, location)
-    #     # simulate for different star transformation fraction
-    #     STF_list = [0.2, 0.3, 0.4, 1.0, 1.4, 1.5]  # Run time: 2172.6556639671326
-    #     imf = 'igimf'
-    #     pool = mp.Pool(mp.cpu_count())
-    #     pool.map(a_pipeline, [STF for STF in STF_list])
-    #     pool.close()
+    # a_pipeline
+    # igimf   1.0 2.0 3.0            50   (0.2 0.3) 0.4 1.0 1.4 1.5
+    # generate SFH:
+    SFEN = 50  # Parallelizing only work for the same SFEN since SFH.txt file is the same!
+    SFH_shape = 'flat'
+    location = 0
+    skewness = 10
+    sfr_tail = 0
+    Log_SFR_list = [3.0]
+    for Log_SFR in Log_SFR_list:
+        galevo.generate_SFH(SFH_shape, Log_SFR, SFEN, sfr_tail, skewness, location)
+        # simulate for different star transformation fraction
+        STF_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5]  # Run time: 2172.6556639671326
+        imf = 'igimf'
+        pool = mp.Pool(mp.cpu_count())
+        pool.map(a_pipeline, [STF for STF in STF_list])
+        pool.close()
 
     # # a_pipeline
     # STF_list = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5]
@@ -132,26 +132,26 @@ if __name__ == '__main__':
     # pool.close()
 
 
-    # # a_pipeline_pair
-    # generate SFH:
-    SFEN = 400  # Parallelizing only work for the same SFEN since SFH.txt file is the same!
-    SFH_shape = 'flat'
-    location = 0
-    skewness = 10
-    sfr_tail = 0
-    input_parameters_list = []
-    imf_list = ['igimf', 'Kroupa']
-    STF_list = [0.1, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5]
-    for imf in imf_list:
-        for STF in STF_list:
-            input_parameters_list.append([imf, STF])
-    Log_SFR_list = [-1.0, 0.0, 1.0, 2.0, 3.0]
-    for Log_SFR in Log_SFR_list:
-        galevo.generate_SFH(SFH_shape, Log_SFR, SFEN, sfr_tail, skewness, location)
-        # simulate for different star transformation fraction
-        pool = mp.Pool(mp.cpu_count())
-        pool.map(a_pipeline_pair, [parameters for parameters in input_parameters_list])
-        pool.close()
+    # # # a_pipeline_pair
+    # # generate SFH:
+    # SFEN = 50  # Parallelizing only work for the same SFEN since SFH.txt file is the same!
+    # SFH_shape = 'flat'
+    # location = 0
+    # skewness = 10
+    # sfr_tail = 0
+    # input_parameters_list = []
+    # imf_list = ['igimf', 'Kroupa']
+    # STF_list = [0.1, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5]
+    # for imf in imf_list:
+    #     for STF in STF_list:
+    #         input_parameters_list.append([imf, STF])
+    # Log_SFR_list = [-1.0, 0.0, 1.0, 2.0, 3.0]
+    # for Log_SFR in Log_SFR_list:
+    #     galevo.generate_SFH(SFH_shape, Log_SFR, SFEN, sfr_tail, skewness, location)
+    #     # simulate for different star transformation fraction
+    #     pool = mp.Pool(mp.cpu_count())
+    #     pool.map(a_pipeline_pair, [parameters for parameters in input_parameters_list])
+    #     pool.close()
 
 
 
